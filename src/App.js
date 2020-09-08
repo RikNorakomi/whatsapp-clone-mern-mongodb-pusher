a17c5bbac9331f03f4e7d918f5440c0363b39935
rikvanvelzen <rikvanvelzen@yahoo.com>
2020-09-08 11:14:23 +0200

    pusher updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Pusher from "pusher";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("a87a1c1a2aec326fcd61", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
   return (
     // BEM naming convention
     <div className="app">
       <div className="app__body">
         <Sidebar />
         <Chat />
+        left off at 3:18:57
       </div>
     </div>
   );
